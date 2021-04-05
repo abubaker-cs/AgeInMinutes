3f6a540804604a112c8296b90821627304573167
abubaker <mohammad_abubaker@yahoo.com>
2021-04-05 21:11:11 +0500

    working...

--- app/src/main/java/org/abubaker/ageinminutes/MainActivity.kt
+++ app/src/main/java/org/abubaker/ageinminutes/MainActivity.kt
@@ -2,10 +2,18 @@ package org.abubaker.ageinminutes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnDatePicker = findViewById<Button>(R.id.btnDatePicker)
+
     }
+
+
 }
