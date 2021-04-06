fa6fe375c61ad2bee922cc3a04feb6ff13395c09
abubaker <mohammad_abubaker@yahoo.com>
2021-04-07 01:37:34 +0500

    working...

--- app/src/main/java/org/abubaker/ageinminutes/MainActivity.kt
+++ app/src/main/java/org/abubaker/ageinminutes/MainActivity.kt
@@ -1,18 +1,49 @@
 package org.abubaker.ageinminutes
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.Toast
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // It will load activity_main.xml view
         setContentView(R.layout.activity_main)
 
+        // Select Button from XML file
         val btnDatePicker = findViewById<Button>(R.id.btnDatePicker)
 
+        // onClick Event
+        btnDatePicker.setOnClickListener { view ->
+            clickDatePicker(view)
+            // Toast.makeText(this, "Button works", Toast.LENGTH_LONG).show()
+        }
+
+
+    }
+
+    // DatePicker
+    fun clickDatePicker(view: View) {
+
+        val myCalendar = Calendar.getInstance()
+        val year = myCalendar.get(Calendar.YEAR)
+        val month = myCalendar.get(Calendar.MONTH)
+        val day = myCalendar.get(Calendar.DAY_OF_MONTH)
+
+        DatePickerDialog(
+            this,
+            DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth -> },
+            year,
+            month,
+            day
+        ).show()
     }
 
 
